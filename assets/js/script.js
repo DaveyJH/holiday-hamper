a537a5519dc3d0389151ddcce28cb5efe93b557e
Mike Avgeros <mike.avgeros@gmail.com>
2021-12-11 14:47:12 +0000

    Added functionality to toggle the burger icon.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,34 @@
 // -------------------------------------------------------------------- Navbar 
-
-let burgerIcon = document.getElementById("burger-icon");
-let closeIcon = document.getElementById("close-icon");
+const toggle = document.querySelector("#burger-icon");
 let navbar = document.getElementById("menu");
+let open = false;
 
-burgerIcon.addEventListener("click", toggleNavbar);
-// closeIcon.addEventListener("click", toggleNavbar);
+toggle.addEventListener("click", toggleNavbar);
 
 function toggleNavbar() {
+    open = !open;
+    open ? closeNavBar() : openNavBar();
     if (navbar.classList.contains("navbar-toggle")) {
         navbar.classList.remove("navbar-toggle");
     } else {
         navbar.classList.add("navbar-toggle");
     }
+}
+
+function openNavBar() {
+    toggle.innerHTML = 
+    `
+    <span class="sr-only">Open the navigation menu</span>
+    <i class="fas fa-bars fa-2x tap-target bar-icon"></i>
+    `;
+}
+
+function closeNavBar() {
+    toggle.innerHTML = 
+    `
+    <span class="sr-only">Close the navigation menu</span>
+    <i class="fas fa-times fa-2x tap-target bar-icon"></i>
+    `;
 }
 
 // -------------------------------------------------------------------- Copyright
