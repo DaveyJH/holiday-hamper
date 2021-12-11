b6b32c5b6b5e89b8e9073bb885aab361b93a3588
Abibubble <abi.yamaha.neo@gmail.com>
2021-12-11 19:46:48 +0000

    wip: Fix JS console errors

---
+++ assets/js/btt-btn.js
@@ -0,0 +1,5 @@
+// -------------------------------------------------------------------- Go To Top Button
+
+window.addEventListener('scroll', function () {
+    scroll.classList.toggle('active', window.scrollY > 500);
+});
